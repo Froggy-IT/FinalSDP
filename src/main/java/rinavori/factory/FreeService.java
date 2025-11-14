d84206d9e1a32e3133f10e359d18e9adcbdd79f7
Dikosh <aldi12062006@gmail.com>
2025-11-15 03:36:48 +0500

    Update play method message in FreeService'

--- src/main/java/rinavori/factory/FreeService.java
+++ src/main/java/rinavori/factory/FreeService.java
@@ -3,6 +3,6 @@ package rinavori.factory;
 public class FreeService implements StreamingService {
     @Override
     public void play() {
-        System.out.println("Free Service: with ads.");
+        System.out.println("Service: Free plan — ads may appear and bitrate is lower.");
     }
 }
