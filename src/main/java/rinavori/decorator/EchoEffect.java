2e434bd7eb2669ba7de5c395aecfb4bf9def95ac
Dikosh <aldi12062006@gmail.com>
2025-11-16 11:45:38 +0500

    Refactor EchoEffect class for cleaner implementation

--- src/main/java/rinavori/decorator/EchoEffect.java
+++ src/main/java/rinavori/decorator/EchoEffect.java
@@ -1,12 +1,9 @@
 package rinavori.decorator;
-
 public class EchoEffect implements TrackEffect {
     private final TrackEffect inner;
     public EchoEffect(TrackEffect inner) { this.inner = inner; }
-
     @Override
     public String describe() { return inner.describe() + " + Echo"; }
-
     @Override
     public void apply() {
         inner.apply();
