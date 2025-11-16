806cb18e4e3746e1bb61862b76e6539b86c30cf0
Dikosh <aldi12062006@gmail.com>
2025-11-16 11:47:07 +0500

    Remove unnecessary blank line in ServiceFactory

--- src/main/java/rinavori/factory/ServiceFactory.java
+++ src/main/java/rinavori/factory/ServiceFactory.java
@@ -1,5 +1,4 @@
 package rinavori.factory;
-
 public class ServiceFactory {
     public StreamingService getService(String type) {
         if ("premium".equalsIgnoreCase(type)) return new PremiumService();
